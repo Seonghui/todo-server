55f875c56e2f337478f2a78c8a5ab690ee9e5a3a
Seonghui <stellashyou@gmail.com>
2021-01-16 14:32:25 +0900

    port 변수 선언 변경

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,15 @@ import env from '@/src/config/enviromnent';
 dotEnvFlow.config();
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 console.log(`NODE_ENV=${env.NODE_ENV}`);
 
-app.listen(env.PORT, () => {
-  console.log(`Server is running on port ${env.PORT}.`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
 
 app.use(router);
